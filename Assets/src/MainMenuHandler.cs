448ceee2b7b983668f1c67b761c7f89b83b8dbf9
Jesse Pitel <jesse.pitel@gmail.com>
2014-10-14 23:57:02 -1000

    Merging ship selection screen and ability selection screen. Selection screens will accomodate up to 4 players. No buttons on selection screens work yet.

--- Assets/src/MainMenuHandler.cs
+++ Assets/src/MainMenuHandler.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MainMenuHandler : MonoBehaviour {
+
+	int playerNumber;
 
 	// Use this for initialization
 	void Start () {
@@ -22,8 +25,15 @@ public class MainMenuHandler : MonoBehaviour {
 		}
 	}
 
+	public void ChangeSlider(float players) {
+
+		this.playerNumber = Mathf.RoundToInt(players);
+		GameObject.Find("NumberOfPlayers").GetComponent<Text>().text = "Players: " + this.playerNumber.ToString();
+	}
+
 	void SelectShip() {
 
+		GameValues.numberOfPlayers = playerNumber;
 		Application.LoadLevel("ShipSelection");
 	}
 }
