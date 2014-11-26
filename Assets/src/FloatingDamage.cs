a316795680c507061e6d52ca705dc2a49ac7e08a
Justin Smith <jtsmith1287@gmail.com>
2014-11-26 01:50:31 -0800

    Adding standard floating damage
    This functions at the lowest level and does not need to be implemented per source of damage. Any damage inflicted to a Destructable will display the damage text.
    
    https://app.asana.com/0/19050287655194/21090038366112

---
+++ Assets/src/FloatingDamage.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FloatingDamage : MonoBehaviour {
+
+    public float FadeSpeed;
+    float DriftAngle;
+
+    void Start() {
+
+        transform.Rotate(new Vector3(90f, 0f, 0f));
+        transform.position += new Vector3(0f, 0f, 1.5f);
+        StartCoroutine(Tick());
+    }
+
+    private IEnumerator Tick() {
+
+        float ticker = 0f;
+
+        CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
+
+        while (ticker < FadeSpeed) {
+            ticker += Time.deltaTime;
+            canvasGroup.alpha -= Time.deltaTime / .75f;
+            transform.position += new Vector3(0f, 0f, 2 * Time.deltaTime);
+            yield return new WaitForFixedUpdate();
+        }
+
+        Destroy(gameObject);
+    }
+}
