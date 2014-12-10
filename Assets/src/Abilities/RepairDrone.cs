5d789d6ac05972063aee0e430256b209c64b0e79
Justin Smith <jtsmith1287@gmail.com>
2014-12-09 22:39:11 -0800

    Added ShieldCover ability, renamed health and dissipation to Armor and Heat and modified a few abilities to work without shields.

---
+++ Assets/src/Abilities/RepairDrone.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class RepairDrone : BaseAbility, IAbility{	
+	
+	public void Start() {
+		
+		Cost = 75f;
+		PrimaryEffect = 75;
+	}
+	
+	public void Begin(ShipObject ship){
+		
+		Ship = ship;
+		ShipMove = ship.GetComponent<ShipMovement>();
+		ShipClass = ship.ShipClass;
+		StartCoroutine(Execute());
+	}
+	
+	public IEnumerator Execute(){
+		
+		Setup();
+		foreach(ShipObject ship in SceneHandler.PlayerShips){
+			ship.RestoreArmor(PrimaryEffect);
+		}
+		yield return new WaitForEndOfFrame();
+		TearDown();
+	}
+	
+	public void Setup(){
+		
+		Executing = true;
+	}
+	
+	public void TearDown(){
+		
+		Executing = false;
+		DurationTimer = 0f;
+	}
+	
+	public void TriggerEnter(Collider collider){}
+	
+	public void TriggerStay(Collider collider){}
+	
+	public void TriggerExit(Collider collider){}
+}
