f532a8c01f8cf9344e2213a89d7cb49a909b5da9
Justin Smith <jtsmith1287@gmail.com>
2014-12-14 16:04:37 -0800

    Modifiers are reimplemented.

---
+++ Assets/src/Abilities/Utils/BaseModifierHandler.cs
@@ -0,0 +1,37 @@
+﻿﻿using UnityEngine;
+using System.Collections;
+
+public class BaseModifierHandler : MonoBehaviour {
+
+	ShipObject Ship;
+
+	/// <summary>
+	/// Checks if a specific Modifier exists on the ship, and if it does replaces the new modifier with that object.
+	/// </summary>
+	/// <param name="newMod">Check if this object exists</param>
+	/// <param name="oldMod">Object to replace if newMod exists already</param>
+	/// <returns></returns>
+	public bool Exists(Modifier newMod, out Modifier oldMod) {
+
+		bool exists = false;
+
+		foreach (Modifier mod in newMod.Ship.ActiveBoons) {
+			if (mod.ID == newMod.ID) {
+				exists = true;
+				oldMod = mod;
+			}
+		}
+
+		if (!exists) {
+			foreach (Modifier mod in newMod.Ship.ActiveConditions) {
+				if (mod.ID == newMod.ID) {
+					exists = true;
+					oldMod = mod;
+				}
+			}
+		}
+
+		oldMod = newMod;
+		return exists;
+	}
+}
---
+++ Assets/src/Abilities/Utils/Modifier.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// Ship modifier object representing a positive or negative effect on a Ship.
+/// The Modifier will handle it's own timer and remove itself automatically.
+/// </summary>
+public class Modifier {
+
+	public ShipObject Ship;
+	public AbilityID ID;
+	public float Duration;
+	public float DurationTimer = 0f;
+	public float Mod;
+	public bool Stacking;
+	public Boon Boon;
+	public Condition Condition;
+
+
+	public Modifier(
+			ShipObject ship,
+			AbilityID id,
+			float duration,
+			float mod,
+			Boon boon,
+			bool stacking = false) {
+
+		Ship = ship;
+		ID = id;
+		Duration = duration;
+		Mod = mod;
+		Boon = boon;
+		Stacking = stacking;
+	}
+
+	public Modifier(
+			ShipObject ship,
+			AbilityID id,
+			float duration,
+			float mod,
+			Condition condition,
+			bool stacking = false) {
+
+		Ship = ship;
+		ID = id;
+		Duration = duration;
+		Mod = mod;
+		Condition = condition;
+		Stacking = stacking;
+
+	}
+}
+
