3e6a35910626e3144c62fdcec9d9521fc838adbc
Justin Smith <jtsmith1287@gmail.com>
2014-12-17 22:47:53 -0800

    Fix: Salvage Conversion Rounds not tracking to target https://app.asana.com/0/21183874779936/21220675257057

--- Assets/Resources/PlayerShips/ShipObjects/ShipObjectScripts/SalvageConversionRoundsProjectile.cs
+++ Assets/Resources/PlayerShips/ShipObjects/ShipObjectScripts/SalvageConversionRoundsProjectile.cs
@@ -3,17 +3,21 @@ using System.Collections;
 
 public class SalvageConversionRoundsProjectile : Projectile, IProjectile {
 
-	
-	void Update () {
-		
-		// TODO: Needs to work with all angles.
-		transform.position += transform.TransformDirection(this.velocity * Time.deltaTime * Direction);
-			
+	void Update() {
+
+		if (Target != null) {
+			Direction = Vector3.Normalize(Target.transform.position - transform.position);
+			transform.position += this.velocity * Direction * Time.deltaTime;
+		} else {
+			// TODO: Needs to work with all angles.
+			transform.position += transform.TransformDirection(this.velocity * Time.deltaTime * Direction);
+		}
+
 		Vector3 positionToCamera = Camera.main.WorldToViewportPoint(transform.position);
-		
-		if (positionToCamera.x > 1.5f | positionToCamera.x < -0.5f){
+
+		if (positionToCamera.x > 1.5f | positionToCamera.x < -0.5f) {
 			Destroy(gameObject);
-		} else if (positionToCamera.y > 1.5f | positionToCamera.y < -0.5){
+		} else if (positionToCamera.y > 1.5f | positionToCamera.y < -0.5) {
 			Destroy(gameObject);
 		}
 	}
