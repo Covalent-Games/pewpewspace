f724be7d31f8ead996a5cbf7932fe0b29d4fee53
Jesse Pitel <jesse.pitel@gmail.com>
2015-02-21 19:55:13 -1000

    Bull Rush effect. Sustain Drone effect.
    https://app.asana.com/0/26383712672946/26894005899674
    https://app.asana.com/0/26383712672946/26894005899672

--- Assets/Resources/Abilities/BullRush/BullRush.cs
+++ Assets/Resources/Abilities/BullRush/BullRush.cs
@@ -12,6 +12,7 @@ public class BullRush : BaseAbility, IAbility {
 		Cost = 50f;
 		Duration = 0.3f;
 		Name = "Bull Rush";
+		Resource = Resources.Load("Effects/BullRushEffect");
 	}
 
 	public void Begin(ShipObject ship) {
@@ -26,12 +27,24 @@ public class BullRush : BaseAbility, IAbility {
 	public IEnumerator Execute() {
 
 		Setup();
+
+		GameObject BullRushEffectGO = (GameObject)Instantiate(Resource, Ship.transform.position, Ship.transform.rotation);
+		BullRushEffectGO.transform.parent = Ship.transform;
+		BullRushEffectGO.transform.Rotate(180, 0, 0);
+		BullRushEffectGO.transform.localPosition = new Vector3(0f, 0f, 1.05f);
+
 		while (DurationTimer < Duration) {
 			DurationTimer += Time.deltaTime;
 			ShipMove.MoveShip(Vector3.MoveTowards(Ship.transform.position, moveTowards, Time.deltaTime * ShipMove.moveSpeed * 4));
 			yield return new WaitForEndOfFrame();
 		}
+
 		TearDown();
+
+		BullRushEffectGO.GetComponent<ParticleSystem>().Stop();
+		yield return new WaitForSeconds(BullRushEffectGO.GetComponent<ParticleSystem>().startLifetime);
+		Destroy(BullRushEffectGO);
+
 	}
 
 	public void Setup() {
--- Assets/Resources/Abilities/SustainDrone/SustainDrone.cs
+++ Assets/Resources/Abilities/SustainDrone/SustainDrone.cs
@@ -13,6 +13,7 @@ public class SustainDrone : BaseAbility, IAbility {
 		Cost = 45f;
 		Duration = 8f;
 		PrimaryEffect = 6;
+		Resource = Resources.Load("Abilities/SustainDrone/SustainDroneObject");
 	}
 
 	public void Begin(ShipObject ship) {
@@ -31,12 +32,57 @@ public class SustainDrone : BaseAbility, IAbility {
 		}
 
 		Setup();
-		while (enabled) {
+
+		GameObject SustainDroneGO = (GameObject)Instantiate(Resource, Ship.transform.position, Ship.transform.rotation);
+		SustainDroneGO.transform.parent = Ship.transform;
+
+		StartCoroutine(RotateDrone(SustainDroneGO, Random.rotation));
+
+		float time = Time.time;
+		while (DurationTimer < Duration) {
+			DurationTimer += Time.time - time;
+			time = Time.time;
+
 			Ship.RestoreArmor(PrimaryEffect * UpdateFrequency);
 			yield return new WaitForSeconds(UpdateFrequency);
 		}
 
+		Destroy(SustainDroneGO);
+
 		TearDown();
+	}
+
+	public IEnumerator DroneSpark(GameObject drone) {
+
+		LineRenderer spark = drone.GetComponentInChildren<LineRenderer>();
+		spark.SetPosition(0, Ship.transform.position);
+		float waitTime;
+		while (spark) {
+			waitTime = Random.Range(0.2f, 1f);
+			yield return new WaitForSeconds(waitTime);
+			spark.enabled = !spark.enabled;
+		}
+	}
+
+	public IEnumerator RotateDrone(GameObject drone, Quaternion nextRotation) {
+
+		bool hasZapped = false;
+		float startTime = Time.time;
+		float journeyLength = 5f;
+		while (drone) {
+			float distCovered = (Time.time - startTime) * 3;
+			float fracJourney = distCovered / journeyLength;
+			drone.transform.rotation = Quaternion.Lerp(drone.transform.rotation, nextRotation, fracJourney);
+			if (fracJourney >= 0.9 && !hasZapped) {
+				Debug.Log("**ZAP!**");
+				hasZapped = true;
+			}
+			if (fracJourney >= 1) {
+				StartCoroutine(RotateDrone(drone, Random.rotation));
+				break;
+			}
+			yield return null;
+		}
 	}
 
 	public void Setup() {
